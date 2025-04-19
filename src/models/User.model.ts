ac240de1546ce73b05c2082ffd157deab3fadabe
“Sourav <elsesourav@gmail.com>
2025-04-19 19:57:31 +0530

    setup models and zod schemas

---
+++ src/models/User.model.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Message, MessageSchema } from "./Message.model";
+
+export interface User extends Document {
+   username: string;
+   email: string;
+   password: string;
+   verifyCode: string;
+   verifyCodeExpiry: Date;
+   isVerified: boolean;
+   isAdmin: boolean;
+   isAcceptingMessage: boolean;
+   messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+   username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      unique: true,
+      match: [/^[a-zA-Z0-9_]{3,20}$/, "Please provide a valid username"],
+   },
+   email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please provide a valid email"],
+   },
+   password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+   },
+   verifyCode: {
+      type: String,
+      required: [true, "Verify code is required"],
+   },
+   verifyCodeExpiry: {
+      type: Date,
+      required: [true, "Verify code Expiry is required"],
+   },
+   isVerified: {
+      type: Boolean,
+      default: false,
+   },
+   isAdmin: {
+      type: Boolean,
+      default: false,
+   },
+   isAcceptingMessage: {
+      type: Boolean,
+      default: true,
+   },
+   messages: [MessageSchema],
+});
+
+const UserModel =
+   (mongoose.models.User as mongoose.Model<User>) ||
+   mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
---
+++ src/schemas/message.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const acceptMessageSchema = z.object({
+   content: z
+      .string()
+      .min(4, { message: "Content must be at least of 4 characters" })
+      .max(300, { message: "Content must be no more then 300 characters" }),
+});
